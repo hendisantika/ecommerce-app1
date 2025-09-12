de5411420ca8c931f0eac3d86121e1e43c7d070b
hendisantika <hendisantika@yahoo.co.id>
2025-09-13 05:24:20 +0700

    feat: Init Project🫸🌀✏️📗🐧🐳⬆

---
+++ src/test/java/id/my/hendisantika/ecommerceapp1/EcommerceApp1ApplicationTests.java
@@ -0,0 +1,13 @@
+package id.my.hendisantika.ecommerceapp1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcommerceApp1ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
