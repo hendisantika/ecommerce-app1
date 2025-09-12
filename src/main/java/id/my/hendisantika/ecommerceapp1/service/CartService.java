20f5cd0c204b6a384af79dfbc4b4c8f6fe222c88
hendisantika <hendisantika@yahoo.co.id>
2025-09-13 05:39:10 +0700

    feat: Add CartService🫸🌀✏️📗🐧🐳⬆

---
+++ src/main/java/id/my/hendisantika/ecommerceapp1/service/CartService.java
@@ -0,0 +1,48 @@
+package id.my.hendisantika.ecommerceapp1.service;
+
+import id.my.hendisantika.ecommerceapp1.entity.Cart;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : ecommerce-app1
+ * User: hendisantika
+ * Link: s.id/hendisantika
+ * Email: hendisantika@yahoo.co.id
+ * Telegram : @hendisantika34
+ * Date: 13/09/25
+ * Time: 05.38
+ * To change this template use File | Settings | File Templates.
+ */
+@Service
+public class CartService {
+    @Autowired
+    private cartDao cartDao;
+
+    public Cart addCart(Cart cart) {
+        return cartDao.addCart(cart);
+    }
+
+    //    public Cart getCart(int id)
+//    {
+//        return cartDao.getCart(id);
+//    }
+    public List<Cart> getCarts() {
+        return this.cartDao.getCarts();
+    }
+
+    public void updateCart(Cart cart) {
+        cartDao.updateCart(cart);
+    }
+
+    public void deleteCart(Cart cart) {
+        cartDao.deleteCart(cart);
+    }
+
+//    pubiic List<Cart> getCartByUserId(int customer_id){
+//        return cartDao.getCartsByCustomerID(customer_id);
+//    }
+}
